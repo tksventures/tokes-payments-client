dd11307cbc356b8317e9b375aba71c2b33f2f437
Michal Mikolajczyk <michal@vikinggarage.com>
2019-09-25 00:00:27 +0200

    minor semantic change #4

--- src/payments.js
+++ src/payments.js
@@ -131,7 +131,7 @@ function Payments(config) {
 
     kycContactsPost: (contactData, accesskey) => post('/api/kyc/contacts', contactData, accesskey),
 
-    kycAccountsGet: accesskey => get('/api/kyc/accounts', accesskey),
+    kycAccountGet: accesskey => get('/api/kyc/accounts', accesskey),
 
     kycAccountsPost: (accountData, accesskey) => post('/api/kyc/accounts', accountData, accesskey),
   };
