c3e28e4ed3229d692fbddf44635c17c5d642fc8b
Michal Mikolajczyk <michal@vikinggarage.com>
2020-01-03 14:46:27 +0100

    complete kyc request list #4

--- src/payments.js
+++ src/payments.js
@@ -4,32 +4,32 @@ function Payments(config) {
   const { host, apiKey, serviceKey } = config;
 
   if (!host) throw new Error('Payments host definition is required');
+  if (!apiKey) throw new Error('The API key is required');
 
   async function get(path, accesskey = apiKey) {
-    try {
-      const { data } = await axios.get(`${host}${path}`, {
-        headers: { accesskey, appkey: serviceKey || 'unsigned' },
-      });
-      return data;
-    } catch (error) {
-      throw new Error(error);
-    }
+    const { data } = await axios.get(`${host}${path}`, {
+      headers: { accesskey, appkey: serviceKey || 'unsigned' },
+    });
+    return data;
   }
 
   async function post(path, postData, accesskey = apiKey) {
-    try {
-      const { data } = await axios.post(`${host}${path}`, postData, {
-        headers: { accesskey, appkey: serviceKey || 'unsigned' },
-      });
-      return data;
-    } catch (error) {
-      throw new Error(error);
-    }
+    const { data } = await axios.post(`${host}${path}`, postData, {
+      headers: { accesskey, appkey: serviceKey || 'unsigned' },
+    });
+    return data;
   }
 
   async function put(path, putData, accesskey = apiKey) {
+    const { data } = await axios.put(`${host}${path}`, putData, {
+      headers: { accesskey, appkey: serviceKey || 'unsigned' },
+    });
+    return data;
+  }
+
+  async function patch(path, patchData, accesskey = apiKey) {
     try {
-      const { data } = await axios.put(`${host}${path}`, putData, {
+      const { data } = await axios.patch(`${host}${path}`, patchData, {
         headers: { accesskey, appkey: serviceKey || 'unsigned' },
       });
       return data;
@@ -39,24 +39,27 @@ function Payments(config) {
   }
 
   async function del(path, accesskey = apiKey) {
-    try {
-      const { data } = await axios.delete(`${host}${path}`, {
-        headers: { accesskey, appkey: serviceKey || 'unsigned' },
-      });
-      return data;
-    } catch (error) {
-      throw new Error(error);
-    }
+    const { data } = await axios.delete(`${host}${path}`, {
+      headers: { accesskey, appkey: serviceKey || 'unsigned' },
+    });
+    return data;
+  }
+
+  async function delNoData(path, accesskey = apiKey) {
+    const response = await axios.delete(`${host}${path}`, {
+      headers: { accesskey, appkey: serviceKey || 'unsigned' },
+    });
+    return response;
   }
 
   const PaymentsAPI = {
     test: () => get('/'),
 
-    testKey: accesskey => get('/api/test', accesskey),
+    testKey: (accesskey) => get('/api/test', accesskey),
 
-    createMerchant: userData => post('/api/merchant', userData),
+    createMerchant: (userData) => post('/api/merchant', userData),
 
-    renewKey: accesskey => post('/api/merchant/key', {}, accesskey),
+    renewKey: (accesskey) => post('/api/merchant/key', {}, accesskey),
 
     stageOrder: (orderData, accesskey) => post('/api/merchant/orders', orderData, accesskey),
 
@@ -76,7 +79,7 @@ function Payments(config) {
 
     rateOrder: (data, accesskey) => put(`/api/merchant/rate/${data.id}`, data, accesskey),
 
-    tokesWithdrawalAddress: accesskey => get('/api/merchant/address', accesskey),
+    tokesWithdrawalAddress: (accesskey) => get('/api/merchant/address', accesskey),
 
     withdrawalAddress: (currency, accesskey) => get(`/api/merchant/address/${currency || ''}`, accesskey),
 
@@ -88,7 +91,9 @@ function Payments(config) {
 
     salesRecords: (days, accesskey) => get(`/api/merchant/records?days=${days}`, accesskey),
 
-    accountData: accesskey => get('/api/account', accesskey),
+    accountData: (accesskey) => get('/api/account', accesskey),
+
+    questionnaireData: (accesskey) => get('/api/questionnaires', accesskey),
 
     accountLookup: (table, id, accesskey) => get(`/api/account/${table}/${id}`, accesskey),
 
@@ -110,11 +115,11 @@ function Payments(config) {
 
     async getFilteredAddresses(accesskey) {
       const addresses = await PaymentsAPI.withdrawalAddress(null, accesskey);
-      const addressArray = Object.keys(addresses).map(key => ({
+      const addressArray = Object.keys(addresses).map((key) => ({
         currency: key,
         address: addresses[key],
       }));
-      const filteredAddressArray = addressArray.filter(address => !!address.address);
+      const filteredAddressArray = addressArray.filter((address) => !!address.address);
 
       return filteredAddressArray;
     },
@@ -125,15 +130,24 @@ function Payments(config) {
       return userAddresess && userAddresess.length;
     },
 
-    kycContactsGet: accesskey => get(`/api/kyc/contacts`, accesskey),
+    kycContactsGet: (accesskey) => get('/api/kyc/contacts', accesskey),
 
-    kycContactsGetAll: accesskey => get(`/api/kyc/contacts/related`, accesskey),
+    kycContactsGetAll: (accesskey) => get('/api/kyc/contacts/related', accesskey),
+
+    kycContactsGetOne: (id, accesskey) => get(`/api/kyc/contacts/${id}`, accesskey),
+
+    kycContactsDelete: (id, accesskey) => delNoData(`/api/kyc/contacts/${id}`, accesskey),
 
     kycContactsPost: (contactData, accesskey) => post('/api/kyc/contacts', contactData, accesskey),
 
-    kycAccountGet: accesskey => get('/api/kyc/accounts', accesskey),
+    kycContactsPatch: (id, contactData, accesskey) => patch(`/api/kyc/contacts/${id}`, contactData, accesskey),
+
+    kycAccountGet: (accesskey) => get('/api/kyc/accounts', accesskey),
+
+    kycQuestionnaireGet: (accesskey) => get('/api/kyc/questionnaire', accesskey),
 
     kycAccountsPost: (accountData, accesskey) => post('/api/kyc/accounts', accountData, accesskey),
+
   };
 
   return PaymentsAPI;
